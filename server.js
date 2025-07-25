cf20ae834f92c730c9abe4fe6b7951f92e5aaa8d
Elenzoo <sergiuszzyromski@gmail.com>
2025-07-25 18:49:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const { startYouTubeChat } = require("./ytChatReader");
+//const express = require("express");
+//const http = require("http");
+//const { Server } = require("socket.io");
+//const { startYouTubeChat } = require("./ytChatReader");
 
 const app = express();
 const server = http.createServer(app);
